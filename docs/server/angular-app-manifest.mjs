709dbf8ccc6453fc210a89874e10ca2830c0d2e0
Luke Serafin <lserafin@alumni.cmu.edu>
2026-07-29 16:06:34 -0400

    New build to fix broken publications link.

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 1028, hash: '3b560a22a621cd23a9c476f314d90234511bc58be88052e2d00da8199d76cccc', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1363, hash: 'bd5168be0d904ad57bb9e3c772e40a6c9ccf3a77a742120963991a657aa2286d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1028, hash: '6bed45547379c910d3f958fc9544e7fe429c98f285982dc6083ab036f37a10ad', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1363, hash: '28a0c05f7942a1412ab19f1165888d1567dabacdbaaddca75c66b04892d5a1c0', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-6XP47TV4.css': {size: 134, hash: 'oMEU2SjqfU8', text: () => import('./assets-chunks/styles-6XP47TV4_css.mjs').then(m => m.default)}
   },
 };
